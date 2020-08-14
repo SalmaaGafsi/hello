9e2a2176ee7b4b28a52e711570471ab932b0824f
SalmaGafsi <68271628+SalmaaGafsi@users.noreply.github.com>
2020-08-14 11:23:06 +0200

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,8 @@
 public class hello {
 
 
-public static void main(string[] args) {
+public static void main(String[] args) {
     System.out.println("hello world ");
-    System.out.println("hello hello");
 
     
 }
